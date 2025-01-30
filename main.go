2ff1c5cda453e102f8a85bf9d8bef324e95c60de
Okta-jpg <oktavianuschn@gmail.com>
2025-01-30 19:54:59 +0700

    PROGRES : CRUD PRODUCT

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"go-web-native/config"
 	"go-web-native/controllers/categorycontroller"
 	"go-web-native/controllers/homecontroller"
+	"go-web-native/controllers/productcontroller"
 	"log"
 	"net/http"
 )
@@ -16,6 +17,12 @@ func main() {
 	http.HandleFunc("/categories/add", categorycontroller.Add)
 	http.HandleFunc("/categories/edit", categorycontroller.Edit)
 	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
 
 	log.Println("Server running on port http://localhost:8080")
 	http.ListenAndServe("127.0.0.1:8080", nil)
